00b01a4ba37fa0923bf4c22e65426054c3e3633a
jared frankel <jaredfrankel10@gmail.com>
2018-02-05 18:52:00 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20180124220843) do
      t.datetime "created_at",    null: false
      t.datetime "updated_at",    null: false
    end
- 
+
    add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
    add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
    add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
- 
+
    create_table "admin_users", force: :cascade do |t|
      t.string   "email",                  default: "", null: false
      t.string   "encrypted_password",     default: "", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20180124220843) do
      t.datetime "created_at",                          null: false
      t.datetime "updated_at",                          null: false
    end
- 
+
    add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
    add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
