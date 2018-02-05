75c9259c61dcd543d70fc8f8a79e7dee65fa4d0f
jared frankel <jaredfrankel10@gmail.com>
2018-02-05 18:42:38 +0000

    automatically backed up by learn

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Dashboard" do
  
    menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
- 
+
    content title: proc{ I18n.t("active_admin.dashboard") } do
      div class: "blank_slate_container", id: "dashboard_default_message" do
        span class: "blank_slate" do
@@ -9,7 +9,7 @@ ActiveAdmin.register_page "Dashboard" do
          small I18n.t("active_admin.dashboard_welcome.call_to_action")
        end
      end
- 
+
      # Here is an example of a simple dashboard with columns and panels.
      #
      # columns do
@@ -22,7 +22,7 @@ ActiveAdmin.register_page "Dashboard" do
      #       end
      #     end
      #   end
- 
+
      #   column do
      #     panel "Info" do
      #       para "Welcome to ActiveAdmin."
